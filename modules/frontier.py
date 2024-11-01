ca63077fe935b7c32820cecaa1658d28a8d1c78a
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2024-11-01 16:53:04 +0530

    Updated the frontier module

--- modules/frontier.py
+++ modules/frontier.py
@@ -1,2 +1,19 @@
+from queue import PriorityQueue
+
+
 class URLFrontier:
-    pass
+    def __init__(self):
+        self.frontier = PriorityQueue()
+        self.visited = set()
+
+    def add_url(self, url, priority=0):
+        if url not in self.visited:
+            self.frontier.put((priority,url))
+            self.visited.add(url)
+    
+    def has_next(self):
+        return not self.frontier.empty()
+    
+    def get_next_url(self):
+        fetched_priority, fetched_url = self.frontier.get()
+        return fetched_url
