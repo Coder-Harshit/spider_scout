2e9424da9e77bc43af5129a101809ab84ad8361c
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2024-11-01 17:01:51 +0530

    upgraded the downloader module functionality
    added the headers and even auto-incrementing delay to allow the page to properly reload before giving error & even the max-attempts before exiting

--- modules/downloader.py
+++ modules/downloader.py
@@ -1,12 +1,20 @@
 import requests
+import time
 
 class Downloader:
-    def fetch(self, url):
-        try:
-            response = requests.get(url)
-            response.raise_for_status()
-            return response.text
-        
-        except requests.exceptions.RequestException as err:
-            print(f"Error Fetching {url}: {err}")
+    def __init__(self, user_agent, delay=1):
+        self.user_agent = user_agent
+        self.delay = delay
+
+    def fetch(self, url, max_attempts=3):
+        headers = {'User-Agent':self.user_agent}
+        for attempt in range(max_attempts):
+            try:
+                response = requests.get(url, headers=headers)
+                response.raise_for_status()
+                return response.text
+            
+            except requests.exceptions.RequestException as err:
+                print(f"Error Fetching {url}: {err}")
+                time.sleep(self.delay * (2**attempt))
             return None
